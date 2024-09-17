145e22c2a7e615c7fa571a3b6e29fa80754371a3
trong.kd <kienductrong@gmail.com>
2024-09-17 08:09:45 +0700

    init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./views/Login";
+import Register from "./views/Register";
+import ForgotPassword from "./views/ForgotPassword";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
