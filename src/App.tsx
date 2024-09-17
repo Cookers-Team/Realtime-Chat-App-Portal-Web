f91f10ae3825be72d4f3f0e04757fe9c0cafd61d
trong.kd <kienductrong@gmail.com>
2024-09-17 10:19:49 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./views/Login";
 import Register from "./views/Register";
 import ForgotPassword from "./views/ForgotPassword";
-import { remoteUrl } from "./types/constant";
 
 const App = () => {
   return (
-    <Router basename={remoteUrl}>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
