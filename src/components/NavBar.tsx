9d334915539bf42e97294bdb49c6305106e3691b
vantrungit <137430777+vantrung1109@users.noreply.github.com>
2024-10-15 08:03:02 +0700

    Revert "sign_out_dialog"
    This reverts commit 0a50f36f4b08b9398f7b910cb8b745773304319a.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -105,7 +105,6 @@ const NavBar: React.FC<NavBarProps> = ({
         onConfirm={onConfirmLogout}
         onCancel={onCancelLogout}
         confirmText="Đăng xuất"
-        color="red"
       />
 
       <Tooltip id="tooltip-profile" style={{ zIndex: 100 }} />
